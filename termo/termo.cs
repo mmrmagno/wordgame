6fddf32341c994e49b2edf02fbe974834fcfb4bd
mmrmagno <mmrmagno@gmail.com>
2022-02-14 15:45:58 +0100

    Design Update & Features
    Still not perfect but getting there

--- termo/termo.cs
+++ termo/termo.cs
@@ -22,8 +22,9 @@ public class Program
     {
    
         Boolean repeat = true;
+        Boolean replay = false;
 
-        while (repeat)
+        while (repeat || replay)
         {
             
                 try
@@ -36,10 +37,18 @@ public class Program
                     
                         Console.WriteLine(title);
                         
-                        string[] words = new string[] {"abuse", "adult", "agent", "anger", "apple", "award", "basis", "beach", "birth", "block", "blood", "board", "brain", "bread", "break", "brown", "buyer", "cause", "chain", "chair", "chest", "chief", "child", "china", "claim", "class", "clock", "coach", "coast", "court", "cover", "cream", "crime", "cross", "crowd", "crown", "cycle", "dance", "death", "depth", "doubt", "draft", "drama", "dream", "dress", "drink", "drive", "earth", "enemy", "entry", "error", "event", "faith", "fault", "field", "fight", "final", "floor", "focus", "force", "frame", "frank", "front", "fruit", "glass", "grant", "grass", "green", "group", "guide", "heart", "henry", "horse", "hotel", "house", "image", "index", "input", "issue", "japan", "jones", "judge", "knife", "laura", "layer", "level", "lewis", "light", "limit", "lunch", "major", "march", "match", "metal", "model", "money", "month", "motor", "mouth", "music", "night", "noise", "north", "novel", "nurse", "offer", "order", "other", "owner", "panel", "paper", "party", "peace", "peter", "phase", "phone", "piece", "pilot", "pitch", "place", "plane", "plant", "plate", "point", "pound", "power", "press", "price", "pride", "prize", "proof", "queen", "radio", "range", "ratio", "reply", "right", "river", "round", "route", "rugby", "scale", "scene", "scope", "score", "sense", "shape", "share", "sheep", "sheet", "shift", "shirt", "shock", "sight", "simon", "skill", "sleep", "smile", "smith", "smoke", "sound", "south", "space", "speed", "spite", "sport", "squad", "staff", "stage", "start", "state", "steam", "steel", "stock", "stone", "store", "study", "stuff", "style", "sugar", "table", "taste", "terry", "theme", "thing", "title", "total", "touch", "tower", "track", "trade", "train", "trend", "trial", "trust", "truth", "uncle", "union", "unity", "value", "video", "visit", "voice", "waste", "watch", "water", "while", "white", "whole", "woman", "world", "youth"};
+                      
+                      string[] words = new string[] {"abuse", "adult", "agent", "anger", "apple", "award", "basis", "beach", "birth", "block", "blood", "board", "brain", "bread", "break", "brown", "buyer", "cause", "chain", "chair", "chest", "chief", "child", "china", "claim",
+                       "class", "clock", "coach", "coast", "court", "cover", "cream", "crime", "cross", "crowd", "crown", "cycle", "dance", "death", "depth", "doubt", "draft", "drama", "dream", "dress", "drink", "drive", "earth", "enemy", "entry", "error", "event", "faith",
+                       "fault", "field", "fight", "final", "floor", "focus", "force", "frame", "frank", "front", "fruit", "glass", "grant", "grass", "green", "group", "guide", "heart", "henry", "horse", "hotel", "house", "image", "index", "input", "issue", "japan", "jones",
+                       "judge", "knife", "laura", "layer", "level", "lewis", "light", "limit", "lunch", "major", "march", "match", "metal", "model", "money", "month", "motor", "mouth", "music", "night", "noise", "north", "novel", "nurse", "offer", "order", "other", "owner",
+                       "panel", "paper", "party", "peace", "peter", "phase", "phone", "piece", "pilot", "pitch", "place", "plane", "plant", "plate", "point", "pound", "power", "press", "price", "pride", "prize", "proof", "queen", "radio", "range", "ratio", "reply", "right",
+                       "river", "round", "route", "rugby", "scale", "scene", "scope", "score", "sense", "shape", "share", "sheep", "sheet", "shift", "shirt", "shock", "sight", "simon", "skill", "sleep", "smile", "smith", "smoke", "sound", "south", "space", "speed", "spite",
+                       "sport", "squad", "staff", "stage", "start", "state", "steam", "steel", "stock", "stone", "store", "study", "stuff", "style", "sugar", "table", "taste", "terry", "theme", "thing", "title", "total", "touch", "tower", "track", "trade", "train", "trend",
+                       "trial", "trust", "truth", "uncle", "union", "unity", "value", "video", "visit", "voice", "waste", "watch", "water", "while", "white", "whole", "woman", "world", "youth"};
                         Random random = new Random();
                         int randomWordPickPos = random.Next(0, words.Length);
-                        string solution = words[0];
+                        string solution = words[randomWordPickPos];
 
             
                         int guessNum = 8;
@@ -58,22 +67,24 @@ public class Program
                                 if (guess[i] == solution[i])
                                 {
                                     result[i] = LetterCorrection.Correct;
-                                    Console.ForegroundColor = ConsoleColor.Green;
-                                    Console.Write(guess[i]);
+                                    Console.BackgroundColor = ConsoleColor.Green;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Write(" " + guess.ToUpper()[i] + " ");
                                     Console.ResetColor();
                                 }
                                 else if (solution.Contains(guess[i]))
                                 {
                                     result[i] = LetterCorrection.WrongPosition;
-                                    Console.ForegroundColor = ConsoleColor.Yellow;
-                                    Console.Write(guess[i]);                                    
+                                    Console.BackgroundColor = ConsoleColor.Yellow;
+                                    Console.ForegroundColor = ConsoleColor.Black;
+                                    Console.Write(" " + guess.ToUpper()[i] + " ");                                    
                                     Console.ResetColor();
                                 }
                                 else
                                 {
                                     result[i] = LetterCorrection.Wrong;
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.Write(guess[i]);
+                                    Console.BackgroundColor = ConsoleColor.Red;
+                                    Console.Write(" " + guess.ToUpper()[i] + " ");
                                     Console.ResetColor();
                                 }
 
@@ -81,14 +92,28 @@ public class Program
 
                           if (guess == solution)
                           {
+                              Console.WriteLine();
                               break;
-                          }  
+                          }
 
                         }
 
                         Console.WriteLine();
-                        Console.WriteLine(solution);
+                        Console.WriteLine("the word was:" + solution);
                         repeat = false;
+                        Console.WriteLine("Would you like to go again?? [y|n]");
+                        string replayString = Console.ReadLine();
+                        if (replayString == "y")
+                        {
+                            replay = true;
+                        }
+                        else
+                        {
+                            replay = false;
+                            Console.WriteLine("See you next time!!");
+                            break;
+                        }
+                        
         
                     }
                     catch (System.Exception e)
